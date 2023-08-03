b594f3eb266a773c138ac6ab296ad972f814a202
Benyamin <khaleghibenyamin@gmail.com>
2023-08-03 13:27:03 +0330

    End of section 22

--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -90,7 +90,7 @@ namespace Infrastructure.Services
 
             await _unitOfWork.Complete();
 
-            return null;
+            return order;
         }
 
         public async Task<Order> UpdateOrderPaymentFailed(string paymentIntentId)
